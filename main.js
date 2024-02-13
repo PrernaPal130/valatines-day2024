f48cc35efe7ac6c47ace584a0e4ad67e6b7a31f1
PrernaPal130 <148892410+PrernaPal130@users.noreply.github.com>
2024-02-14 00:59:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function yes() {
+    document.getElementById('question').style.display = 'none';
+    document.getElementById('yesorno').style.display = 'none';
+    document.getElementById('yesgif').style.display = 'block';
+    document.getElementById('cutegif').style.display='none';
+}
+
+function no() {
+    document.getElementById('question').style.display = 'none';
+    document.getElementById('yesorno').style.display = 'none';
+    document.getElementById('nogif').style.display = 'block';
+    document.getElementById('cutegif').style.display='none';
+}
